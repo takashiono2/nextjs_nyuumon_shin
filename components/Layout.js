caebe25d7be456e5eb33bd7f7825f019085334a8
takashiono2 <takashiono710@gmail.com>
2023-06-05 01:48:14 +0900

    section4_21

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import styles from "./layout.module.css"
+import utilstyles from "../styles/utils.module.css"
+
+const name = "Shin Code";
+export const siteTitle = "Next.js blog";
+
+const Layout = ({children}) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header className={styles.header}>
+        <img src="/images/profile.png" className={utilstyles.borderCircle}/>
+        <h1 className={utilstyles.heading2Xl}>{ name }</h1>
+      </header>
+      <main>{children}</main>
+    </div>
+  )
+}
+
+export default Layout
